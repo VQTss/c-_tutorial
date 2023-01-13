f02fcd5b662c1e5f297335df1e895f55a0fec869
Thai Vo <thaivo.software@gmail.com>
2023-01-13 16:27:58 +0700

    additional finally in program.cs

--- cs19/Program.cs
+++ cs19/Program.cs
@@ -22,34 +22,36 @@ namespace cs19
         {
 
 
-            try
-            {
-                Register("Vo Quoc Thai ",15);
-            }
-            catch (NameEmptyException nne)
-            {
-                Console.WriteLine(nne.Message);
-            }
-            catch(AgeException a ){
-                Console.WriteLine(a.Message);
-                a.AgeExceptionDetail();
-            }
-
-
-
-
             // try
             // {
-            //     int[] arr = new int[] { 4, 5, 8 };
-            //     Console.Write(arr[8]);
+            //     Register("Vo Quoc Thai ",15);
             // }
-            // catch (Exception e)
+            // catch (NameEmptyException nne)
             // {
-            //     Console.WriteLine(e.Message);
-            //     Console.WriteLine(e.StackTrace);
-            //     Console.WriteLine(e.GetType().Name);
-            //     Console.WriteLine("An error occurred");
+            //     Console.WriteLine(nne.Message);
             // }
+            // catch(AgeException a ){
+            //     Console.WriteLine(a.Message);
+            //     a.AgeExceptionDetail();
+            // }
+
+
+
+
+            try
+            {
+                int[] arr = new int[] { 4, 5, 8 };
+                Console.Write(arr[8]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.GetType().Name);
+                Console.WriteLine("An error occurred");
+            }finally {
+                Console.WriteLine("End");
+            }
         }
     }
 }
