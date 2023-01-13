b2659ccdf83d4b0b17e5fd43edfbc4117b7059c1
Thai Vo <thaivo.software@gmail.com>
2023-01-13 14:30:34 +0700

    add cs18 about enum

---
+++ cs18/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
