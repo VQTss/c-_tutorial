75f5833056184df483f68deb142e701d7f4522ce
Thai Vo <thaivo.software@gmail.com>
2023-01-13 14:28:53 +0700

    Add cs17 about struct datastructure

--- cs17/Program.cs
+++ cs17/Program.cs
@@ -2,42 +2,86 @@
 
 namespace cs17
 {
-    //  truct là kiểu tham trị
+    //  struct là kiểu tham trị
     class Program
     {
-        
-        public struct product{
+
+        public struct product
+        {
             // du lieu
 
             public string name;
             public double price;
             // phuong thuc
 
-            public string getInfor(){
+            public string getInfor()
+            {
                 return $"Name: {name} , Price: {price}";
             }
 
 
             // contructor
-            public product(string name, double price){
+            public product(string name, double price)
+            {
                 this.name = name;
                 this.price = price;
             }
 
 
         }
-        
 
-        
+
+        public class product2
+        {
+            // du lieu
+
+            public string name;
+            public double price;
+            // phuong thuc
+
+            public string getInfor()
+            {
+                return $"Name: {name} , Price: {price}";
+            }
+
+
+            // contructor
+            public product2(string name, double price)
+            {
+                this.name = name;
+                this.price = price;
+            }
+
+
+        }
+
         static void Main(string[] args)
         {
-           product sanpam1;
-        
-            sanpam1.name  = "Iphone";
+            // use struct
+            product sanpam1;
+            sanpam1.name = "Iphone";
             sanpam1.price = 500;
+            product sanpham2 = new product("Samsung", 4000);
+            sanpham2 = sanpam1;
+            sanpham2.name = "Iphone X";
             Console.WriteLine($"{sanpam1.getInfor()}");
-            product sanpham2 =  new product("Samsung",4000);
             Console.WriteLine($"{sanpham2.getInfor()}");
+            // this is using compare struct and class 
+            // we are having two product and product2
+            // Class là tham chiếu
+            // khi sử dụng những cấu trúc dữ liệu đơn giản nhỏ dùng struct và ngược lại thì dùng class
+            product2 p = new product2("",0);
+            p.name = "Samsung";
+            p.price = 400;
+
+            product2 p2 =  new product2("Noikia",200);
+            p2 = p;
+            p2.name ="SamSung Galaxy";
+            Console.WriteLine($"{p.getInfor()}");
+            Console.WriteLine($"{p2.getInfor()}");
+
+
+
         }
     }
 }
