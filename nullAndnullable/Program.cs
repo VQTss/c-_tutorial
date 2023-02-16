87c0a1e5f66b5a6ac6fcc41dc95c157d79c2af97
Thai Vo <thaivo.software@gmail.com>
2023-02-16 11:38:53 +0700

    null , nullable

---
+++ nullAndnullable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace nullAndnullable
+{
+    class Program
+    {
+
+        // null and nullable
+        /*
+        
+        
+        */
+        class ABC
+        {
+            public  void Hello(){
+                Console.WriteLine("Xin chao c#");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+                Có 2 trường hợp xảy ra null
+                Không khởi tạo đối tượng thì xảy ra null
+                và gán nó bằng null
+            */
+            ABC a = null; // như thế này sẽ bị lỗi 
+            if (a != null){
+                a.Hello();
+            } // ==  a?.Hello();
+           
+        }
+    }
+}
